1ed793f65f68f6babe46c593712734dbe1f36a26
stephanec <stephanec@washingmachine>
2016-11-27 19:37:37 +0100

    a bit of concurrency

--- registration.go
+++ registration.go
@@ -3,6 +3,7 @@ package ud859
 import (
 	"golang.org/x/net/context"
 
+	"google.golang.org/appengine"
 	"google.golang.org/appengine/datastore"
 )
 
@@ -41,16 +42,36 @@ func (ConferenceAPI) GotoConference(c context.Context, form *ConferenceKeyForm) 
 	}
 
 	return datastore.RunInTransaction(c, func(c context.Context) error {
-		// get the profile
-		profile, err := getProfile(c, pid)
-		if err != nil {
-			return err
-		}
+		errc := make(chan error)
+		var profile *Profile
+		var conference *Conference
 
-		// get the conference
-		conference, err := getConference(c, ckey)
-		if err != nil {
-			return errBadRequest(err, "conference does not exist")
+		go func() {
+			// get the profile
+			var err error
+			profile, err = getProfile(c, pid)
+			errc <- err
+		}()
+
+		go func() {
+			// get the conference
+			var err error
+			conference, err = getConference(c, ckey)
+			if err != nil {
+				err = errBadRequest(err, "conference does not exist")
+			}
+			errc <- err
+		}()
+
+		// wait and check for errors
+		multi := make(appengine.MultiError, 0, 2)
+		for i := 0; i < 2; i++ {
+			if err := <-errc; err != nil {
+				multi = append(multi, err)
+			}
+		}
+		if len(multi) > 0 {
+			return multi
 		}
 
 		if profile.HasRegistered(conference.WebsafeKey) {
@@ -87,16 +108,36 @@ func (ConferenceAPI) CancelConference(c context.Context, form *ConferenceKeyForm
 	}
 
 	return datastore.RunInTransaction(c, func(c context.Context) error {
-		// get the profile
-		profile, err := getProfile(c, pid)
-		if err != nil {
-			return err
-		}
+		errc := make(chan error)
+		var profile *Profile
+		var conference *Conference
 
-		// get the conference
-		conference, err := getConference(c, ckey)
-		if err != nil {
-			return errBadRequest(err, "conference does not exist")
+		go func() {
+			// get the profile
+			var err error
+			profile, err = getProfile(c, pid)
+			errc <- err
+		}()
+
+		go func() {
+			// get the conference
+			var err error
+			conference, err = getConference(c, ckey)
+			if err != nil {
+				err = errBadRequest(err, "conference does not exist")
+			}
+			errc <- err
+		}()
+
+		// wait and check for errors
+		multi := make(appengine.MultiError, 0, 2)
+		for i := 0; i < 2; i++ {
+			if err := <-errc; err != nil {
+				multi = append(multi, err)
+			}
+		}
+		if len(multi) > 0 {
+			return multi
 		}
 
 		if !profile.HasRegistered(conference.WebsafeKey) {
